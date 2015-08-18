a13669679ac9e4da084f607af2b539e505de98ce
TNM3 <tnm3@TNM3s-Mac-mini.local>
2015-08-18 17:31:33 +0530

    Initial Commit

---
+++ synax/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  synax
+//
+//  Created by TNM3 on 8/18/15.
+//  Copyright (c) 2015 TNM3. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
